fd76ff74a5bd33fdb260c61de0f7027985d0bc69
caleb <kalejaiyecaleb@gmail.com>
2024-07-03 11:53:23 +0100

    commit

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+const currentTime = document.getElementById("currentTimeUTC")
+const currentDay = document.getElementById("currentDay")
 
 
 function updateUTCTime() {
     const now = new Date();
     const utcTime = now.toUTCString();
 
-    document.getElementById('currentTimeUTC').innerHTML = utcTime;
+    currentTime.innerHTML = utcTime;
 }
 updateUTCTime();
+
+function updateCurrentDay() {
+    const now = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayName = days[now.getUTCDay()];
+
+    currentDay.innerHTML = dayName;
+}
+
+updateCurrentDay();
+setInterval(updateUTCTime, 1000)
